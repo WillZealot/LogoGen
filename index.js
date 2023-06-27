8a91c669e78c0ee67cdb0d1d04e201f75d7aac86
william <williamtikhonenko@gmail.com>
2023-06-27 16:38:46 -0400

    added commetns for peices of the code and functions as well as creating an examplle Logo folder for final submission

--- index.js
+++ index.js
@@ -1,67 +1,93 @@
-const fs = require('fs'); //file system module for reading and writing files
-const inquirer = require('inquirer');// inquirer module for prompting user
-const {Circle, Triangle, Square} = require('./lib/shapes');// importing the logo generating function
+// Import the required modules and shape classes
+const fs = require('fs'); // File system module for reading and writing files
+const inquirer = require('inquirer'); // Inquirer module for prompting user
+const { Circle, Triangle, Square } = require('./lib/shapes'); // Importing the shape classes
 
 // Array of questions for the user to provide project details
 const questions = [
-    { name: 'logotext', message: 'Select the three text characters for your logo !',validate: (input) => {
+  {
+    name: 'logotext',
+    message: 'Select the three text characters for your logo!',
+    validate: (input) => {
       if (input.length <= 3) {
         return true;
       }
       return 'Please enter up to three characters.';
-    }, },
-    { name: 'txtcolor', message: 'Enter which color you would like for the text ! Color KeyWord Or Hexadecimal!' },
-    { name: 'shape', message: 'Which shape would you like?', type: 'checkbox', choices: [
+    },
+  },
+  {
+    name: 'txtcolor',
+    message: 'Enter which color you would like for the text! Color Keyword or Hexadecimal!',
+  },
+  {
+    name: 'shape',
+    message: 'Which shape would you like?',
+    type: 'checkbox',
+    choices: [
       'Triangle',
       'Circle',
       'Square',
-    ]},
-    { name: 'shapecolor', message: 'Enter which color you would like for your shape ! Color KeyWord Or Hexadecimal!' },
-  ];
-  
+    ],
+  },
+  {
+    name: 'shapecolor',
+    message: 'Enter which color you would like for your shape! Color Keyword or Hexadecimal!',
+  },
+];
+
+// Function to create the SVG logo
+const createSvg = () => {
   inquirer
-  .prompt(questions)
-  .then((data) => {
-    //TODO: Generate logo content using user's answers
-    path = 'newLogo.svg';
-    let answers = data;
-    
-    let shapetype;
-    switch (answers.shape[0]) {
-      case 'Circle':
-        shapetype = new Circle();
-        break;
-      case 'Square':
-        shapetype = new Square();
-        break;
+    .prompt(questions)
+    .then((data) => {
+      // TODO: Generate logo content using user's answers
+
+      const path = 'newLogo.svg'; // Path to the output SVG file
+      const answers = data; // User's answers from inquirer prompt
+
+      let shapetype;
+
+      // Determine the selected shape based on user's choice
+      switch (answers.shape[0]) {
+        case 'Circle':
+          shapetype = new Circle(); // Create a new instance of the Circle class
+          break;
+        case 'Square':
+          shapetype = new Square(); // Create a new instance of the Square class
+          break;
         case 'Triangle':
-        shapetype = new Triangle();
-        break;
-      default:
-        break;
-    }
+          shapetype = new Triangle(); // Create a new instance of the Triangle class
+          break;
+        default:
+          break;
+      }
 
-    shapetype.setColor(answers.shapecolor);
-    // 
-    const svg = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-    ${shapetype.render()}
-    <text x="50%" y="50%" fill="${answers.txtcolor}" text-anchor="middle" alignment-baseline="middle" font-family="Arial" font-size="48">${answers.logotext}</text>
-  </svg>`;
-  
-  fs.writeFile(path, svg, (err) => {
-    if (err) {
-      console.error('Logo Not Generated! Heres Why..:', err);
-    } else {
-      console.log(`Logo Created in newlogo.svg`);
-    }
-  });
+      shapetype.setColor(answers.shapecolor); // Set the color of the selected shape
 
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      console.error('Error:', error);
-    } else {
-      // Something else went wrong
-    }
-  });
+      // Construct the SVG content using the selected shape, text, and colors
+      const svg = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+          ${shapetype.render()} <!-- Render the selected shape -->
+          <text x="50%" y="50%" fill="${answers.txtcolor}" text-anchor="middle" alignment-baseline="middle" font-family="Arial" font-size="48">${answers.logotext}</text>
+        </svg>`;
+
+      // Write the SVG content to the output file
+      fs.writeFile(path, svg, (err) => {
+        if (err) {
+          console.error('Logo Not Generated! Here\'s Why:', err);
+        } else {
+          console.log(`Logo Created in newLogo.svg`);
+        }
+      });
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.error('Error:', error);
+      } else {
+        // Something else went wrong
+      }
+    });
+};
+
+// Call the createSvg function to start the logo creation process
+createSvg();
