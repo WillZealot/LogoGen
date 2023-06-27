654373f66eeeee00a2c250e25c4ca8b8b8c85178
william <williamtikhonenko@gmail.com>
2023-06-27 16:13:03 -0400

    my code now creates a svg with the correct corresponding input i had some trouble with the shapes part but i realized i have to call it from inside an array very cool

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const fs = require('fs'); //file system module for reading and writing files
 const inquirer = require('inquirer');// inquirer module for prompting user
-const generateLogo = require('./lib/shapes');// importing the logo generating function
+const {Circle, Triangle, Square} = require('./lib/shapes');// importing the logo generating function
 
 // Array of questions for the user to provide project details
 const questions = [
-    { name: 'logotext', message: 'Select the three text characters for your logo !' },
+    { name: 'logotext', message: 'Select the three text characters for your logo !',validate: (input) => {
+      if (input.length <= 3) {
+        return true;
+      }
+      return 'Please enter up to three characters.';
+    }, },
     { name: 'txtcolor', message: 'Enter which color you would like for the text ! Color KeyWord Or Hexadecimal!' },
     { name: 'shape', message: 'Which shape would you like?', type: 'checkbox', choices: [
       'Triangle',
@@ -16,18 +21,42 @@ const questions = [
   
   inquirer
   .prompt(questions)
-  .then((answers) => {
+  .then((data) => {
     //TODO: Generate logo content using user's answers
-
-    console.log(answers);
+    path = 'newLogo.svg';
+    let answers = data;
     
-    fs.writeFile('Logo.svg', markdownContent, (err) => { // Write markdown content to README.md file
-      if (err) {
-        console.error('Error creating logo:', err);
-      } else {
-        console.log('Logo file has been successfully generated.');
-      }
-    });
+    let shapetype;
+    switch (answers.shape[0]) {
+      case 'Circle':
+        shapetype = new Circle();
+        break;
+      case 'Square':
+        shapetype = new Square();
+        break;
+        case 'Triangle':
+        shapetype = new Triangle();
+        break;
+      default:
+        break;
+    }
+
+    shapetype.setColor(answers.shapecolor);
+    // 
+    const svg = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+    ${shapetype.render()}
+    <text x="50%" y="50%" fill="${answers.txtcolor}" text-anchor="middle" alignment-baseline="middle" font-family="Arial" font-size="48">${answers.logotext}</text>
+  </svg>`;
+  
+  fs.writeFile(path, svg, (err) => {
+    if (err) {
+      console.error('Logo Not Generated! Heres Why..:', err);
+    } else {
+      console.log(`Logo Created in newlogo.svg`);
+    }
+  });
+
   })
   .catch((error) => {
     if (error.isTtyError) {
